499491cef1f629ec3c3829726878a6162a5ada0a
anastasijas2101 <anastasijas101@gmail.com>
2024-05-28 22:40:50 +0200

    Add seventh page and changes to the first page

--- src/features/Home/components/FifthPage.jsx
+++ src/features/Home/components/FifthPage.jsx
@@ -3,20 +3,20 @@ import Button from '../../../components/Button/Button'
 
 const FifthPage = () => {
     return (
-        <div className='full-page-div1'>
-            <div className='top-page-div'>
+        <div className='fifth-div'>
+            <div className='top-fifth-div'>
                 <h1>Product Information</h1>
-                <div className='input-div'>
+                <div className='input-fifth-div'>
                     <input placeholder='Enter Your Email Here' />
                     <Button buttonText='Get Product Information' />
                 </div>
             </div>
-            <div className='bottom-page-div1'>
+            <div className='bottom-fifth-div'>
                 <h1>Feature your best sellers</h1>
                 <p>There are many variations of passages of Lorem Ipsum available, but majority have suffered you
                     are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden
                     in the middle of text. All the Lorem Ipsum generators.</p>
-                <div className='list-div'>
+                <div className='list-fifth-div'>
                     <ul>
                         <li>It uses a dictionary of over Latin words as necessary</li>
                         <li>Combined with handful of model sentence structure lorem</li>
---
+++ src/features/Home/components/SeventhPage.jsx
@@ -0,0 +1,14 @@
+import '../styles/SeventhPage.style.css'
+import MainPage from './MainPage';
+
+const SeventhPage = () => {
+    return (
+        <div className='seventh-div'>
+            <MainPage />
+            <footer>Copyright © domainname.com. All Rights Reserved |
+                Designed by: buylandingpagdesign.com</footer>
+        </div>
+    )
+}
+
+export default SeventhPage;
